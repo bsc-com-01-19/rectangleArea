79df4dfb3a521bbe1d60fe065afb7d9a1bfba94c
THOKOZANI MAMBERE <BSC/COM/01/19@COMLAB.COM>
2021-12-20 16:51:23 +0200

    the area program has been written

--- Area.java
+++ Area.java
@@ -1,5 +1,25 @@
 public class Area{
 public static void main (String[]args){
+Scanner input = new Scanner(System.in);
 System.out.println("the area will be shown here");
+System.out.print("enter thee length and breadth:");
+int length = input.nextInt();
+int breadth = input.nextInt();
+  
+Area rectangleArea = new Area(length,breadth);
+System.out.println("the area of th rectanglr of length" + length + 
+" and breadth" + breadth + "is"  rectangleArea.getArea());
+}
+}
+class Area{
+int length;
+int breadth;
+ public Area(int length , int breadth){
+this.length = length;
+this.breadth = breadth;
+}
+public double getArea(){
+double area = length * breadth;
+return area;
 }
 }
